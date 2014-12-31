c7d39cf110b43bfd121d9e705f00d305511fe17c
Akshay S Dinesh <asdofindia@gmail.com>
2014-12-31 15:58:20 +0530

    Update 2014-12-31T10:28:20.013Z

--- contact.php
+++ contact.php
@@ -20,6 +20,7 @@ array(
   'email2'                        =>    array('filter' => FILTER_SANITIZE_EMAIL),
   'name2'                            =>    array('filter' => FILTER_SANITIZE_STRING),
   'message2'                            =>    array('filter' => FILTER_SANITIZE_STRING),
+  'captcha'                                     =>    array('filter' => FILTER_SANITIZE_NUMBER_INT)
 );
 
 $revised_post_array = filter_var_array($_POST, $postfilter);    // must be referenced via a variable which is now an array that takes the place of $_POST[]
@@ -37,8 +38,11 @@ function captchapass($captcharesponse){
   return (strcmp($captcharesponse,"4"));
 }
 
+$message = clean_string($revised_post_array["message2"]);
+$captcharesponse = $revised_post_array["captcha"];
+
 if (isWasteString($message)){
-  $finalmessage="Please enter a message";
+  $finalmessage="Please enter a valid message.";
 }
 elseif (!captchapass($captcharesponse)){
   $finalmessage = "Sorry computers are not allowed to email humans.";
@@ -47,7 +51,7 @@ else {
 
   $fromemail = !isWasteString($revised_post_array["email2"]) ? $revised_post_array["email2"] : "anonymous@example.com";
   $fromname = !isWasteString($revised_post_array["name2"]) ? clean_string($revised_post_array["name2"]) : "anonymous";
-  $message = !isWasteString($revised_post_array["message2"]) ? clean_string($revised_post_array["message2"]) : "Sorry! This person sent an empty message :(";
+
 
   $headers = 'From: '.$fromemail."\r\n".
   'Reply-To: '.$fromemail."\r\n" .
@@ -62,10 +66,13 @@ echo <<<EOT
 <html>
   <head>
     <title>Diaspora Yatra</title>
+    <link href="css/contactform.min.css" rel="stylesheet" type='text/css'>
   </head>
   <body>
-    <p>$finalmessage</p>
-    <a href="/">Go back to the campaign page</a>
+    <div id="container">
+      <p id="message">$finalmessage</p>
+      <a id="goback" href="/">Go back to the campaign page</a>
+    </div>
   </body>
 </html>
 EOT;
